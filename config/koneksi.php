8b3e3b80a6f3dde3d8311284729660477d12be76
tripurwantiaskrindo <tripurwanti@askrindo.co.id>
2022-05-19 10:12:08 +0700

    [UPDATE] change pointing db

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$server     = "10.20.10.169";
+$server     = "10.20.10.167";
 $user         = "askrindo";
 $password     = "p@ssw0rd";
 $database     = "aos_kur_bri_dev";
--- config/koneksi_bri_prod.php
+++ config/koneksi_bri_prod.php
@@ -1,8 +1,8 @@
 <?php
-$server 	= "10.20.10.169";
-$user 		= "askrindo";
-$password 	= "p@ssw0rd";
-$database 	= "aos_kur_bri";
+$server     = "10.20.10.169";
+$user         = "askrindo";
+$password     = "p@ssw0rd";
+$database     = "aos_kur_bri";
 
-$con = mssql_connect($server,$user,$password);
-mssql_select_db($database, $con) or die ("Database tidak ditemukan");
+$con = mssql_connect($server, $user, $password);
+mssql_select_db($database, $con) or die("Database tidak ditemukan");
